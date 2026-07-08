2a736143850e9d16154a5ebaba84dcb29c18a22e
Zarif <zarif780.zf@gmail.com>
2026-07-08 05:11:17 +0500

    fixed bugs

--- main.js
+++ main.js
@@ -1,54 +1,45 @@
-const firstProductCard = document.querySelector('.product-card');
-const highlightButton = document.querySelector('#highlight-first-card');
-const greenColorHash = '#00ff00'
-const violetColorHash = '#ff00ff'
-const googleURL = 'http://google.com'
-const outputLogButton = document.querySelector('#output-console-log')
-const backgroundColorButton = document.querySelector('#toggle-own-color')
+const firstProductCard = document.querySelector(".product-card");
+const highlightButton = document.querySelector("#highlight-first-card");
+const greenColorHash = "#00ff00";
+const violetColorHash = "#ff00ff";
+const googleURL = "http://google.com";
+const outputLogButton = document.querySelector("#output-console-log");
 
- highlightButton.addEventListener('click', () => {
-firstProductCard.style.backgroundColor = greenColorHash;
-})
-
-const productCards = document.querySelectorAll('.product-card');
-const highlightAllButton = document.querySelector('#highlight-all-card');
-
- highlightAllButton.addEventListener('click', () => {
-productCards.forEach (card => {
-    card.style.backgroundColor = violetColorHash
+highlightButton.addEventListener("click", () => {
+  firstProductCard.style.backgroundColor = greenColorHash;
 });
- })
 
- const openGoogleButton = document.querySelector('#open-google');
+const productCards = document.querySelectorAll(".product-card");
+const highlightAllButton = document.querySelector("#highlight-all-card");
 
- openGoogleButton.addEventListener('click', () => {
-   window.open(googleURL) 
- })
+highlightAllButton.addEventListener("click", () => {
+  productCards.forEach((card) => {
+    card.style.backgroundColor = violetColorHash;
+  });
+});
 
- outputLogButton.addEventListener('click', ()=> outputConsoleLog('Выбери свой продукт'));
+const openGoogleButton = document.querySelector("#open-google");
 
- function outputConsoleLog(message) {
-    alert(message)
-    console.log(message)
- }
+openGoogleButton.addEventListener("click", () => {
+  window.open(googleURL);
+});
 
- const mainTitle = document.querySelector('.page-title');
- mainTitle.addEventListener('mouseenter', function(event) {
-const titleText = event.target.textContent;
-console.log(titleText);
- });
+outputLogButton.addEventListener("click", () =>
+  outputConsoleLog("Выбери свой продукт"),
+);
 
-backgroundColorButton.style.backgroundColor = 'blue';
-backgroundColorButton.addEventListener('click', function() {
-    if (backgroundColorButton.style.backgroundColor === 'blue') {
-      backgroundColorButton.style.backgroundColor = 'red';
-     } else {
-        backgroundColorButton.style.backgroundColor = 'blue';
-     }
-})
+function outputConsoleLog(message) {
+  alert(message);
+  console.log(message);
+}
 
+const mainTitle = document.querySelector(".page-title");
+mainTitle.addEventListener("mouseenter", function (event) {
+  const titleText = event.target.textContent;
+  console.log(titleText);
+});
 
- 
-
-
-
+const backgroundColorButton = document.getElementById("toggle-own-color");
+backgroundColorButton.addEventListener("click", () => {
+  backgroundColorButton.classList.toggle("red-bg");
+});
