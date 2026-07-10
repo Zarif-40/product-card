fff9de6cef0ef75faa9d883c8122f174a5166b85
Zarif <zarif780.zf@gmail.com>
2026-07-10 06:11:33 +0500

    completed homework

--- homework-7.js
+++ homework-7.js
@@ -1,11 +1,11 @@
-            // 3 пункт.
+// 3 пункт.
 
 const showWeather = (Таразе, temperature) => {
   console.log(`Сейчас в ${Таразе} температура - ${34} градусов по цельсию`);
 };
 showWeather("Таразе", 34);
 
-           // 4 пункт.
+// 4 пункт.
 
 const speedLight = 299792458;
 const checkSpeed = (speed) => {
@@ -23,22 +23,31 @@ checkSpeed(300000000);
 checkSpeed(299792458);
 checkSpeed(250000000);
 
-           // 5 пункт.
+// 5 пункт.
 
 const productName = "Телефон";
 let productPrice = 1000;
 
 const buyProduct = (budget) => {
-    if (budget >= productPrice) {
-        console.log(`${productName} преобретен. Спасибо за покупку!`);
-    } else {
-        const missingAmount = productPrice - budget;
-        console.log(`Вам не хватает ${missingAmount}, пополните баланс`);
-    }
-} 
+  if (budget >= productPrice) {
+    console.log(`${productName} преобретен. Спасибо за покупку!`);
+  } else {
+    const missingAmount = productPrice - budget;
+    console.log(`Вам не хватает ${missingAmount}, пополните баланс`);
+  }
+};
 buyProduct(1500);
 buyProduct(500);
 
-            // 6 пункт.
+// 6 пункт.
 
-function 
+function reminder() {
+  console.log("Нужно позвонить мастеру!");
+}
+reminder();
+
+// 7 пункт.
+
+const cityName = "Midina";
+let population = 2100000;
+let numberMosques = 1000;
