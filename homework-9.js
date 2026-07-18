973a5de4d46197b7aa1a665ec5609095e8438ef7
Zarif <zarif780.zf@gmail.com>
2026-07-18 19:16:38 +0500

    fixed all comments

--- homework-9.js
+++ homework-9.js
@@ -1,3 +1,8 @@
+// 6 пункт.
+
+import { comments } from "./comments.js";
+console.log(comments);
+
 // 2 пункт.
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -7,7 +12,6 @@ console.log(newArray);
 // 3 пункт.
 
 const clothes = ["шорты", "футболка", "джинсы", "рубашка", "носки"];
-clothes.includes("джинсы");
 console.log(clothes.includes("джинсы"));
 
 // 4 пункт.
@@ -18,11 +22,6 @@ const reverseArray = (arr) => {
 console.log(reverseArray(numbers));
 console.log(reverseArray(clothes));
 
-// 6 пункт.
-
-import { comments } from "./comments.js";
-console.log(comments);
-
 // 7 пункт.
 
 const filteredComments = comments.filter((comment) =>
@@ -32,13 +31,9 @@ console.log(filteredComments);
 
 // 8 пункт.
 
-const newComments = comments.map((item) => {
-  const updatedItem = { ...item };
-  if (updatedItem.id <= 5) {
-    updatedItem.postId = 2;
-  } else {
-    updatedItem.postId = 1;
-  }
+const newComments = comments.map((comment) => {
+  const updatedItem = { ...comment };
+  updatedItem.postId = updatedItem.id <= 5 ? 2 : 1;
   return updatedItem;
 });
 console.log(newComments);
