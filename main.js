e22c4e6cead189c170e55951c64951eca16ae9fc
Zarif <zarif780.zf@gmail.com>
2026-08-25 20:23:37 +0500

    creat main.js

--- main.js
+++ main.js
@@ -1,45 +1,8 @@
-const firstProductCard = document.querySelector(".product-card");
-const highlightButton = document.querySelector("#highlight-first-card");
-const greenColorHash = "#00ff00";
-const violetColorHash = "#ff00ff";
-const googleURL = "http://google.com";
-const outputLogButton = document.querySelector("#output-console-log");
-
-highlightButton.addEventListener("click", () => {
-  firstProductCard.style.backgroundColor = greenColorHash;
-});
-
-const productCards = document.querySelectorAll(".product-card");
-const highlightAllButton = document.querySelector("#highlight-all-card");
-
-highlightAllButton.addEventListener("click", () => {
-  productCards.forEach((card) => {
-    card.style.backgroundColor = violetColorHash;
-  });
-});
-
-const openGoogleButton = document.querySelector("#open-google");
-
-openGoogleButton.addEventListener("click", () => {
-  window.open(googleURL);
-});
-
-outputLogButton.addEventListener("click", () =>
-  outputConsoleLog("Выбери свой продукт"),
-);
-
-function outputConsoleLog(message) {
-  alert(message);
-  console.log(message);
-}
-
-const mainTitle = document.querySelector(".page-title");
-mainTitle.addEventListener("mouseenter", function (event) {
-  const titleText = event.target.textContent;
-  console.log(titleText);
-});
-
-const backgroundColorButton = document.getElementById("toggle-own-color");
-backgroundColorButton.addEventListener("click", () => {
-  backgroundColorButton.classList.toggle("red-bg");
-});
+import './comments.js';
+import './products.js';
+import './homework-6.js';
+import './homework-7.js';
+import './homework-8.js';
+import './homework-9.js';
+import './homework-10.js';
+import './homework-11.js';
